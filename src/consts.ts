177d0058df48f5c747302b22a02b400d330d57f7
Eden Collins <ZNAC249@live.rhul.ac.uk>
2026-08-05 12:39:52 +0100

    Header and footer personalisation

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Write-up';
+export const SITE_TITLE = 'Vulnhub Write-ups';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
